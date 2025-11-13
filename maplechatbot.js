5df86cc0d0403c8048e71052044f134b968933c8
reissue <68487086+MinSoo97@users.noreply.github.com>
2025-11-13 09:06:02 +0900

    maplechatbot.js 업데이트

--- maplechatbot.js
+++ maplechatbot.js
@@ -16,14 +16,14 @@ const bot = BotManager.getCurrentBot();
 const API_KEY = "test_379df74dd227525a6c0b85a59f2847ea292cffcf30a97d713f2d8a7cac4ff4cbefe8d04e6d233bd35cf2fabdeb93fb0d";
 const BASE_URL = "https://openai.nexon.com/maplestory/v1";
 
-const TARGET_ROOMS = [ "서브번호", "테스트" ];
+const TARGET_ROOMS = [ "서브번호", "테스트","뺙히릿"];
 
 function onMessage(msg)
 {
   if(!TARGET_ROOMS.includes(msg.room))  return;
-  
+
   const content = msg.content.trim();
-  
+
   //테스트용 하이~
   if(content === "하이")
   {
@@ -58,15 +58,15 @@ function getWeatherFromNaver(msg, msgPart)
     return;
   }
   //request
-  var url = "https://m.search.naver.com/search.naver?query=" + msgPart + "%20날씨"
+  var url = "https://m.search.naver.com/search.naver?query=" + msgPart + "%20날씨";
   var data = org.jsoup.Jsoup.connect(url)
       .header('Referer','https://m.search.naver.com')
       .get();
-  
+
   var result = data.selectFirst('.select_txt').text().trim() + ' 날씨' + '\n'+
                data.selectFirst('.temperature_text').text().trim() + '\n'+
                data.selectFirst('.temperature_info > p').text().trim() +'\n';
-  
+
   msg.reply(result);
 
 }
@@ -117,10 +117,10 @@ function httpGet(msg, url)
  * (string) msg.command: 명령어 이름
  * (Array) msg.args: 명령어 인자 배열
  */
- 
+
 function onCommand(msg) 
 {
-  
+
 }
 
 function onCreate(savedInstanceState, activity) 
