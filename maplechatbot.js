952cbe3253875dd30942f7fafc21e8936e9607b2
reissue <kms33233@gmail.com>
2025-11-20 18:48:56 +0900

    Lotto

--- maplechatbot.js
+++ maplechatbot.js
@@ -188,14 +188,16 @@ function searchLotto(msg, msgPart)
   json.drwtNo5,
   json.drwtNo6
   ];
-
+  var totSellamnt = parseInt(json.totSellamnt, 10);
+  var firstWinamnt = parseInt(json.firstWinamnt, 10);
+  
   var replyMsg = "로또 " + json.drwNo + "회차 결과 \n" +
                  "추첨일: " + json.drwNoDate + "\n\n" +
                  "번호: " + lottoNumbers.join(', ') + "\n" +
                  "보너스: " + json.bnusNo + "\n\n" +
-                 "총 판매금액: " + json.totSellamnt.toLocaleString() + "원\n" +
+                 "총 판매금액: " + totSellamnt.toLocaleString() + "원\n" +
                  "1등 당첨자: " + json.firstPrzwnerCo + "명\n" +
-                 "1등 당첨금: " + json.firstWinamnt.toLocaleString() + "원\n" +
+                 "1등 당첨금: " + firstWinamnt.toLocaleString() + "원\n" +
 
   msg.reply (replyMsg);
 }
